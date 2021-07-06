9e66662d1aa40f39477dc2f87a6181a27a450265
Leandro Martinez <leandro@iqm.unicamp.br>
2021-07-06 16:45:13 -0300

    optimize powers

--- src/forces.jl
+++ src/forces.jl
@@ -9,6 +9,9 @@ Subroutine that computes the force. It modifies the input `f` vector.
 """
 function forces!(f,x,opt)
   @unpack eps, sig, sides = opt
+  sig6 = sig^6
+  sig12 = sig6^2
+  eps4 = 4*eps
   n = length(x)
   for i in 1:n
     f[i] = zero(Point)
@@ -17,8 +20,11 @@ function forces!(f,x,opt)
     for j in i+1:n
       dx = image(x[j] - x[i], sides)
       r = norm(dx)
+      r3 = r^3
+      r7 = r3*r3*r
+      r13 = r7*r3*r3
       drdx = -dx/r
-      dudx = 4*eps*(12*sig^12/r^13 - 6*sig^6/r^7)*drdx
+      dudx = eps4*(12*sig12/r13 - 6*sig6/r7)*drdx
       f[i] = f[i] + dudx 
       f[j] = f[j] - dudx
     end
--- src/potential.jl
+++ src/potential.jl
@@ -6,12 +6,17 @@ Function that computes the potential energy
 function potential(x,opt)
   @unpack sides, eps, sig = opt
   n = length(x)
+  sig6 = sig^6
+  sig12 = sig6^2
+  eps4 = 4*eps
   u = 0.
   for i in 1:n-1
     for j in i+1:n
       dx = image(x[j] - x[i],sides)
-      r = norm(dx)
-      u += 4*eps*(sig^12 / r^12 - sig^6 / r^6)
+      r2 = norm2(dx)
+      r6 = r2^3
+      r12 = r6^2
+      u += eps4*(sig12 / r12 - sig6 / r6)
     end
   end
   return u
