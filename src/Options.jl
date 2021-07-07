93ce153bbe5db2a4ea0650f0871ce13211f27a7a
Leandro Martinez <leandro@iqm.unicamp.br>
2021-07-07 16:49:23 -0300

    up options

--- src/Options.jl
+++ src/Options.jl
@@ -12,50 +12,62 @@ julia> opt = Options(sides=200.,nsteps=2000)
 ```
 
 """
-@with_kw_noshow struct Options{T}
+struct Options{T}
   # Time
-  dt::Float64 = 0.1
-  nsteps::Int = 2000
+  dt::Float64
+  nsteps::Int 
 
   # System parameters
-  sides::T = Point2D(100,100)
-  eps::Float64 = 1.
-  sig::Float64 = 2.
+  sides::T
+  eps::Float64
+  sig::Float64
 
   # Temperature bath parameters
-  kavg_target = 0.6
-  ibath::Int = 1
+  kavg_target
+  ibath::Int
 
   # Output and printing options
-  printxyz::Bool = true
-  printvel::Bool = false
-  iprint::Int = 1
-  iprintxyz::Int = 2
-  trajectory_file::String = "traj.xyz"
-  energies_file::String = "energies.dat"
-  velocities_file::String = "velocities.dat"
+  printxyz::Bool
+  printvel::Bool
+  iprint::Int
+  iprintxyz::Int
+  trajectory_file::String
+  energies_file::String
+  velocities_file::String
 end
 
-# Constructor that allows setting dimension of sides dynamically
-function Options(;kargs...)
-  isides = findfirst(isequal(:sides),keys(kargs))
-  if isides === nothing
-    sides = Point2D(100,100) 
-    return Options{Point2D}(;sides=sides,kargs...)
+function Options(;
+  dt::Float64 = 0.1,
+  nsteps::Int = 2000,
+  sides = nothing,
+  eps::Float64 = 1.,
+  sig::Float64 = 2.,
+  kavg_target = 0.6,
+  ibath::Int = 1,
+  printxyz::Bool = true,
+  printvel::Bool = false,
+  iprint::Int = 1,
+  iprintxyz::Int = 2,
+  trajectory_file::String = "traj.xyz",
+  energies_file::String = "energies.dat",
+  velocities_file::String = "velocities.dat",
+)
+
+  if sides == nothing
+    sides = Point2D(100,100)
   else
-    sides = values(kargs)[isides]
-    kargs = filter(x->x[1] != :sides, kargs)
-    @show kargs
     if length(sides) == 2
       sides = Point2D(sides...)
-      return Options{Point2D}(;sides=sides,kargs...)
     elseif length(sides) == 3
       sides = Point3D(sides...)
-      return Options{Point3D}(;sides=sides,kargs...)
     else
       error("Dimension of sides must be 2 or 3.")
     end
   end
+  Options{typeof(sides)}(dt,nsteps,sides,eps,sig,kavg_target,
+          ibath,printxyz,printvel,iprint,iprintxyz,
+          trajectory_file,energies_file,velocities_file)
+
 end
 
 function Base.show(io::IO,opt::Options)
