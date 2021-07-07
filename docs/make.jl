01586d5ee0cf04d081921e4df42110cc518ff05b
Leandro Martinez <leandro@iqm.unicamp.br>
2021-07-07 12:02:46 -0300

    up make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,32 +1,17 @@
 import Pkg
 Pkg.add("Documenter")
 using Documenter
-using JuliaNotes
+using CELFI
 push!(LOAD_PATH,"../src/")
 makedocs(
-    modules=[JuliaNotes],
-    sitename="JuliaNotes.jl",
+    modules=[CELFI],
+    sitename="CELFI.jl",
     pages = [
         "Home" => "index.md",
-        "Development workflow" => "workflow.md",
-        "Modules and Revise" => "modules.md",
-        "Scope of loops" => "loopscopes.md",
-        "Benchmark" => "benchmark.md",
-        "Assignment and mutation" => "assignment.md",
-        "Type instability" => "instability.md",
-        "Closures" => "closures.md",
-        #"Union splitting" => "splitting.md",
-        "Tracking allocations" => "memory.md",
-        "Immutable variables" => "immutable.md",
-        "ERROR: No method..." => "nomethod.md",
-        "Vector{Int} <: Vector{Real} is false???" => "typevariance.md",
-        "Figures and LaTeX" => "figures.md",
-        "Create new package" => "new_package.md",
-        "Publish Docs" => "publish_docs.md",
     ]
 )
 deploydocs(
-    repo = "github.com/m3g/JuliaNotes.jl.git",
+    repo = "github.com/m3g/CELFI.jl.git",
     target = "build",
     branch = "gh-pages",
     versions = ["stable" => "v^", "v#.#" ],
